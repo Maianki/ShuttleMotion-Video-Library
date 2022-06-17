8485add42d8b0ab10984e7ce7dce0f0db612a565
Ankit Kumain <kumankit10@gmail.com>
2022-06-18 01:06:39 +0530

    style: add responsiveness to like,history,playlist and watch page

--- src/pages/Playlists/Playlists.jsx
+++ src/pages/Playlists/Playlists.jsx
@@ -51,7 +51,7 @@ export function Playlists() {
             className={`btn btn-danger btn-danger ${styles.btnCreatePlaylist}`}
             onClick={btnPlaylistModalHandler}
           >
-            create a new Playlist
+            create playlist
           </button>
         </div>
         <section className={styles.playlists}>
